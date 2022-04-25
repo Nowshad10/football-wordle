3201e96ec93b9fcfbf0a49606582d4113f0286f9
Nowshad Ahmed <nowshad.ahmed@hotmail.co.ukgit config user.name Nowshad10>
2022-04-25 20:06:59 +0100

    Added fetch function to retrieve random player from db

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
+import { useState, useEffect } from 'react';
+
 function App() {
+
+  const [solution, setSolution] = useState(null)
+
+  useEffect(() => {
+    fetch('http://localhost:3001/solutions')
+      .then(response => response.json())
+      .then(data => {
+        const randomSolution = data[Math.floor(Math.random()*data.length)];
+        setSolution(randomSolution.player);
+      })
+  }, [setSolution])
+
   return (
     <div className="App">
-      <h1>Wordle</h1>
+      <h1>Wordle (Football Edition)</h1>
+      { solution && <div>Solution is: {solution.toUpperCase()}</div>}
     </div>
   );
 }
