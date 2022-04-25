0522c5cf0d9ca635af69e0ec53c1bb631c05bba1
Nowshad Ahmed <nowshad.ahmed@hotmail.co.ukgit config user.name Nowshad10>
2022-04-25 21:06:46 +0100

    Added functionality to track user input

--- client/src/components/Wordle.js
+++ client/src/components/Wordle.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import useWordle from '../hooks/useWordle';
 
 const Wordle = ({ solution }) => {
+
+  const { currentGuess, handleKeyUp } = useWordle(solution);
+
+  useEffect(() => {
+    window.addEventListener('keyup', handleKeyUp)
+
+    // cleanup function, prevent numerous events
+    return () => window.removeEventListener('keyup', handleKeyUp);
+  }, [handleKeyUp])
   return (
-    <div>Wordle</div>
+    <div>Current Guess: {currentGuess}</div>
   )
 }
 
--- client/src/hooks/useWordle.js
+++ client/src/hooks/useWordle.js
@@ -9,16 +9,52 @@ const useWordle = (solution) => {
     const [isCorrect, setIsCorrect] = useState(false);
 
     const formatGuess = () => {
-
+        console.log(`Formatting the guess - ${currentGuess}`)
     }
-
+    // add a new guess to the guesses state
+    // update the isCorrect state if guess is correct
+    // add one to the turn state
     const addNewGuess = () => {
 
     }
+    // handle keyup event & track current guess
+    // if user presses enter, add the new guess
+    const handleKeyUp = ({ key }) => {
 
-    const handleKeyUp = () => {
+        if (key === 'Enter') {
+            // only add guess if turn < 5
+            if (turn > 5) {
+                console.log('All turns used up!')
+                return
+            };
+            // do not allow duplicate guesses
+            if (history.includes(currentGuess)) {
+                console.log('Already tried this word')
+                return
+            };
 
-    }
+            if (currentGuess.length !==5) {
+                console.log('Word must be 5 characters long')
+                return
+            };
+
+            formatGuess()
+        }
+        if (key === 'Backspace') {
+            setCurrentGuess((prevState) => {
+                return prevState.slice(0, -1)
+            })
+            return
+        }
+
+        if (/^[A-Za-z]$/.test(key)) {
+            if (currentGuess.length < 5) {
+                setCurrentGuess((prevState) => {
+                    return prevState + key
+                });
+            };
+        };
+    };
     return (
         { turn, currentGuess, guesses, isCorrect, handleKeyUp }
     );
