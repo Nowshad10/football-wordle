7fc736ded9358e8b655162bc668b803799e641b9
Nowshad Ahmed <nowshad.ahmed@hotmail.co.ukgit config user.name Nowshad10>
2022-05-07 14:23:13 +0100

    Displays current guess as user types

--- client/src/components/Row/index.js
+++ client/src/components/Row/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import './style.css';
 
-const Row = ({ guess }) => {
+const Row = ({ guess, currentGuess }) => {
 
   if (guess) {
     return (
       <div className='row past'>
         {guess.map((letter, i) => (
           <div key={i} className={letter.color}>{letter.key}</div>
+        ))}
+      </div>
+    )
+  }
+
+  if (currentGuess) {
+
+    let letters = currentGuess.split('');
+
+    return (
+      <div className='row current'>
+        {letters.map((letter, i) => (
+          <div key={i} className='filled'>{letter}</div>
+        ))}
+        {[...Array(5 - letters.length)].map((value, i) => (
+          <div key={i}></div>
         ))}
       </div>
     )
